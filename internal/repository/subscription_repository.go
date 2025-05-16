2e5a01c09c8242affd955416699241fce8058602
H1vee <ailedinskyi@gmail.com>
2025-05-16 17:07:46 +0300

    Updated Susbscription repository interface

--- internal/repository/subscription_repository.go
+++ internal/repository/subscription_repository.go
@@ -1,8 +1,12 @@
 package repository
 
-import "github.com/H1vee/WeatherAPI/internal/models"
+import (
+	"github.com/H1vee/WeatherAPI/internal/models"
+)
 
 type SubscriptionRepository interface {
 	Create(subscription models.Subscription) error
 	FindByToken(token string) (*models.Subscription, error)
+	UpdateConfirmation(token string, confirmed bool) error
+	Delete(token string) error
 }
